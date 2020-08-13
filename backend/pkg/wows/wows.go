fa237a6e7b01f8182e17a0441e0a4c6b90da2eab
Jan Christophersen <jan.christophersen@digitalunit.berlin>
2020-08-13 13:47:41 +0200

    Use birthday2020 in backend

--- backend/pkg/wows/wows.go
+++ backend/pkg/wows/wows.go
@@ -2,10 +2,10 @@ package wows
 
 // EventStartTime is the Unix timestamp when the event starts
 var EventStartTime = map[string]int{
-	"eu":   1576141200,
-	"com":  1576069200,
-	"ru":   1576047600,
-	"asia": 1576108800,
+	"eu":   1598918400,
+	"com":  1598918400,
+	"ru":   1598918400,
+	"asia": 1598918400,
 }
 
-var ActiveEvent = Snowflake2019{}
+var ActiveEvent = BirthdayEvent2020{}
